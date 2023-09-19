5a43763aeadb9fda4c489a67b79e4276250ca133
alexander sampson <a.sampson206@gmail.com>
2023-09-18 22:14:29 -0700

    wowza

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.querySelector("main").remove("main");
+const newHeader = document.querySelector('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'victory is the champion';
